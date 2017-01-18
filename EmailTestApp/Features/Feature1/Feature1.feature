886671759d6ef688066f06f98ac586649d7fb256
Nešpor Lukáš <lukasnespor@gmail.com>
2017-01-18 11:28:24 +0100

    commit

---
+++ EmailTestApp/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f3b986ed-efbf-4d4c-a8bd-0947b9d83231" description="SharePoint Add-in Feature" featureId="f3b986ed-efbf-4d4c-a8bd-0947b9d83231" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="EmailTestApp Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="22d81aa8-31ef-4a76-874b-8bf5eb14dbc4" />
+    <projectItemReference itemId="6355aa68-f503-4dc1-ae58-1ee1634a59ee" />
+    <projectItemReference itemId="dfa63b53-590e-4ae4-a601-99b141dadc7e" />
+    <projectItemReference itemId="463570c1-408f-4f62-8097-72419d9d8024" />
+  </projectItems>
+</feature>
---
+++ EmailTestApp/Scripts/App.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    $("#recipient").val(_spPageContextInfo.userLoginName);
+
+    $("#send").click(function (e) {
+        e.preventDefault();
+
+        $("#result").html("");
+        var body = $("#body").val();
+        var subject = $("#subject").val();
+        var to = $("#recipient").val().trim().split(";");
+
+        $.ajax({
+            method: "POST",
+            contentType: "application/json",
+            url: _spPageContextInfo.siteAbsoluteUrl + "/_api/SP.Utilities.Utility.SendEmail",
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "Content-Type": "application/json;odata=verbose",
+                "X-RequestDigest": $("#__REQUESTDIGEST").val()
+            },
+            data: JSON.stringify({
+                'properties': {
+                    '__metadata': { 'type': 'SP.Utilities.EmailProperties' },
+                    'To': { 'results': to },
+                    'Body': body,
+                    'Subject': subject
+                }
+            })
+        })
+        .done(function () {
+            $("#result").html("Email sucessfully sent");
+        })
+        .fail(function (xhr, status, error) {
+            var obj = JSON.parse(xhr.responseText);
+            console.error(obj);
+            $("#result").html(obj.error.message.value);
+        });
+    });
+});
